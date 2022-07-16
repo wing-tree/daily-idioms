28760095a2d62a8c9fdfd8377f1752eef98e2d1a
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-16 16:55:51 +0900

    feat : Add constants EMPTY and ONE

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/constant/Constant.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/constant/Constant.kt
@@ -1,5 +1,7 @@
 package com.wing.tree.bruni.daily.idioms.constant
 
+internal const val EMPTY = ""
 internal const val NEWLINE = "\n"
 internal const val OPTION_COUNT = 4
 internal const val ZERO = 0
+internal const val ONE = 1
