65f4393b115d94cd7f7ce0812c3a31f769d65e9b
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-18 03:08:45 +0900

    refactor : Rollback to List from ImmutableList

--- data/src/main/java/com/wing/tree/bruni/daily/idioms/data/entity/Idiom.kt
+++ data/src/main/java/com/wing/tree/bruni/daily/idioms/data/entity/Idiom.kt
@@ -11,7 +11,7 @@ data class Idiom(
     override val id: Int,
     override val category: Int,
     override val chineseCharacters: String,
-    override val chineseMeanings: ImmutableList<String>,
+    override val chineseMeanings: List<String>,
     override val description: String,
     override val koreanCharacters: String,
     override val my: Boolean
