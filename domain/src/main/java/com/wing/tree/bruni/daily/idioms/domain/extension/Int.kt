83569e76f1b072da94034c8dff4ee4f11c50d617
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-17 16:10:36 +0900

    feat : Add Int extension property 'half'

--- domain/src/main/java/com/wing/tree/bruni/daily/idioms/domain/extension/Int.kt
+++ domain/src/main/java/com/wing/tree/bruni/daily/idioms/domain/extension/Int.kt
@@ -1,9 +1,12 @@
 package com.wing.tree.bruni.daily.idioms.domain.extension
 
+import com.wing.tree.bruni.daily.idioms.domain.constant.TWO
 import com.wing.tree.bruni.daily.idioms.domain.constant.ZERO
 
 val Int.isZero: Boolean get() = this == ZERO
 val Int.notZero: Boolean get() = isZero.not()
+
+val Int.half: Int get() = div(TWO)
 
 val Int.char: Char get() = toChar()
 val Int.float: Float get() = toFloat()
