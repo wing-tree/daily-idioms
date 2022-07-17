0a631bfea16c7041355ec2d6a57ecb5277a8ad06
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-18 02:56:26 +0900

    refactor : Change chineseMeanings type to ImmutableList

--- data/src/main/java/com/wing/tree/bruni/daily/idioms/data/entity/Idiom.kt
+++ data/src/main/java/com/wing/tree/bruni/daily/idioms/data/entity/Idiom.kt
@@ -3,6 +3,7 @@ package com.wing.tree.bruni.daily.idioms.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.wing.tree.bruni.daily.idioms.domain.model.Idiom
+import kotlinx.collections.immutable.ImmutableList
 
 @Entity(tableName = "idiom")
 data class Idiom(
@@ -10,7 +11,7 @@ data class Idiom(
     override val id: Int,
     override val category: Int,
     override val chineseCharacters: String,
-    override val chineseMeanings: List<String>,
+    override val chineseMeanings: ImmutableList<String>,
     override val description: String,
     override val koreanCharacters: String,
     override val my: Boolean
