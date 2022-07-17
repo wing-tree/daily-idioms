103d6a5291aaa9bb6df2a390e0133c7dc62587da
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-18 00:32:06 +0900

    refactor : Reorder constant declaration

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/constant/Constant.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/constant/Constant.kt
@@ -2,6 +2,6 @@ package com.wing.tree.bruni.daily.idioms.constant
 
 internal const val EMPTY = ""
 internal const val NEWLINE = "\n"
+internal const val ONE = 1
 internal const val OPTION_COUNT = 4
 internal const val ZERO = 0
-internal const val ONE = 1
