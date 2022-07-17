a2ba85bd64fec04ba3532b54dcb3c9f24c3dbb71
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-17 12:54:24 +0900

    feat : Add char extension property to Int

--- domain/src/main/java/com/wing/tree/bruni/daily/idioms/domain/extension/Int.kt
+++ domain/src/main/java/com/wing/tree/bruni/daily/idioms/domain/extension/Int.kt
@@ -5,4 +5,5 @@ import com.wing.tree.bruni.daily.idioms.domain.constant.ZERO
 val Int.isZero: Boolean get() = this == ZERO
 val Int.notZero: Boolean get() = isZero.not()
 
+val Int.char: Char get() = toChar()
 val Int.float: Float get() = toFloat()
