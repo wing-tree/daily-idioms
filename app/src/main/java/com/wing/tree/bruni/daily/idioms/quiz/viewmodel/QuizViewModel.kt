72b0a9e7ca8badc46c07e8eb0002aaab1c7e9a5a
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-18 00:35:52 +0900

    feat : Add result flow and combine with progress state and add score function

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/viewmodel/QuizViewModel.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/viewmodel/QuizViewModel.kt
@@ -18,6 +18,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
+import java.io.IOException
 import java.util.*
 import javax.inject.Inject
 import com.wing.tree.bruni.daily.idioms.data.entity.Question as Entity
@@ -43,29 +44,45 @@ class QuizViewModel @Inject constructor(
         else -> throw IllegalArgumentException("category :$category")
     }
 
-    val state: StateFlow<QuizState> = flow {
-        withContext(ioDispatcher) {
-            idioms.firstOrNull()?.let { idioms ->
-                questionRepository.deleteAndInsert(generateQuestions(idioms))
+    private val progress = flow {
+        try {
+            withContext(ioDispatcher) {
+                idioms.firstOrNull()?.let { idioms ->
+                    questionRepository.deleteAndInsert(generateQuestions(idioms))
+                }
             }
-        }
 
-        val questionsState = with(questionRepository.allQuestions()) {
-            map {
-                val question = Question.from(it)
-                val isPreviousVisible = it.index.notZero
-                val isDoneVisible = it.index == lastIndex
+            val questionsState = with(questionRepository.allQuestions()) {
+                map {
+                    val question = Question.from(it)
+                    val isPreviousVisible = it.index.notZero
+                    val isDoneVisible = it.index == lastIndex
 
-                QuestionState(
-                    index = question.index,
-                    question = question,
-                    isPreviousVisible = isPreviousVisible,
-                    isDoneVisible = isDoneVisible
-                )
+                    QuestionState(
+                        index = question.index,
+                        question = question,
+                        isPreviousVisible = isPreviousVisible,
+                        isDoneVisible = isDoneVisible
+                    )
+                }
             }
-        }
 
-        emit(QuizState.Progress.Content(questionsState))
+            emit(QuizState.Progress.Content(questionsState))
+        } catch (ioException: IOException) {
+            emit(QuizState.Progress.Error(ioException))
+        }
+    }
+
+    private val result = MutableStateFlow<QuizState.Result>(QuizState.Result.Loading)
+
+    val state: StateFlow<QuizState> = combine(progress, result) { progress, result ->
+        when {
+            result is QuizState.Result.Content -> result
+            result is QuizState.Result.Error -> result
+            progress is QuizState.Progress.Content -> progress
+            progress is QuizState.Progress.Error -> progress
+            else -> QuizState.Progress.Loading
+        }
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
@@ -108,4 +125,20 @@ class QuizViewModel @Inject constructor(
                 }
             }
         }
+
+    fun score(content: QuizState.Progress.Content) {
+        val questionsState = content.questionsState.map {
+            val question = it.question.copy(answer = it.answer)
+
+            it.copy(question = question)
+        }
+
+        val score = content.count
+
+        result.value = QuizState.Result.Content(questionsState)
+    }
+
+    companion object {
+
+    }
 }
