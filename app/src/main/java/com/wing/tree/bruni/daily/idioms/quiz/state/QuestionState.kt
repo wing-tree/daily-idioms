4310afb958abf899c5f712545c7f8aac645a6a58
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-17 16:11:04 +0900

    refactor : Reorder properties of QuestionState

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/state/QuestionState.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/state/QuestionState.kt
@@ -9,8 +9,8 @@ import com.wing.tree.bruni.daily.idioms.quiz.model.Question
 data class QuestionState(
     val index: Int,
     val question: Question,
-    val isDoneVisible: Boolean,
-    val isPreviousVisible: Boolean
+    val isPreviousVisible: Boolean,
+    val isDoneVisible: Boolean
 ) {
     val isNextEnabled: Boolean get() = answer.notNull
     var answer by mutableStateOf(question.answer)
