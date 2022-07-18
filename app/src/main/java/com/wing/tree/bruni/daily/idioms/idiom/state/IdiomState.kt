6b339bf1ae1da23ad2f31646bac214dafe0e7863
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-18 20:09:27 +0900

    refactor : Change property type from List to ImmutableList

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/idiom/state/IdiomState.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/idiom/state/IdiomState.kt
@@ -1,10 +1,11 @@
 package com.wing.tree.bruni.daily.idioms.idiom.state
 
 import com.wing.tree.bruni.daily.idioms.domain.model.Idiom
+import kotlinx.collections.immutable.ImmutableList
 
 sealed interface IdiomState {
     object Loading : IdiomState
-    data class Content(val idioms: List<Idiom>) : IdiomState
+    data class Content(val idioms: ImmutableList<Idiom>) : IdiomState
     data class Error(val throwable: Throwable) : IdiomState
 }
 
