cda59a03ffbe8b86c97aab870d337bead1477959
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-19 00:14:29 +0900

    refactor : Modify ResultState constructor parameter and properties

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/state/ResultState.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/state/ResultState.kt
@@ -2,11 +2,7 @@ package com.wing.tree.bruni.daily.idioms.quiz.state
 
 import com.wing.tree.bruni.daily.idioms.quiz.model.Question
 
-data class ResultState(
-    val index: Int,
-    val question: Question
-) {
+data class ResultState(val question: Question) {
     val answer = question.answer
-    val correctAnswer = question.correctAnswer
-    val isCorrectAnswer = answer == correctAnswer
+    val isCorrectAnswer = answer == question.correctAnswer
 }
