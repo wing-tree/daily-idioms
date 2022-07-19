b5f7dd6ae8ceb16bfa690644debec7f0a732f3e7
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-19 13:37:24 +0900

    feat : Modify TypeConverters for Option class

--- data/src/main/java/com/wing/tree/bruni/daily/idioms/data/typeconverter/TypeConverters.kt
+++ data/src/main/java/com/wing/tree/bruni/daily/idioms/data/typeconverter/TypeConverters.kt
@@ -3,7 +3,7 @@ package com.wing.tree.bruni.daily.idioms.data.typeconverter
 import androidx.room.TypeConverter
 import com.google.gson.*
 import com.google.gson.reflect.TypeToken
-import com.wing.tree.bruni.daily.idioms.data.entity.Idiom
+import com.wing.tree.bruni.daily.idioms.domain.model.Option
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import java.lang.reflect.Type
@@ -22,39 +22,33 @@ class TypeConverters {
 
     @Suppress("HasPlatformType")
     @TypeConverter
-    fun immutableIdiomsToJson(value: ImmutableList<Idiom>) = Gson().toJson(value)
+    fun immutableOptionsToJson(value: ImmutableList<Option>) = Gson().toJson(value)
 
     @TypeConverter
-    fun jsonToImmutableIdioms(value: String): ImmutableList<Idiom> {
-        val type = object : TypeToken<ImmutableList<Idiom>>() {  }.type
+    fun jsonToImmutableOptions(value: String): ImmutableList<Option> {
+        val type = object : TypeToken<ImmutableList<Option>>() {  }.type
 
         val gson = GsonBuilder().registerTypeAdapter(
             type,
-            object : JsonDeserializer<ImmutableList<Idiom>> {
+            object : JsonDeserializer<ImmutableList<Option>> {
                 override fun deserialize(
                     json: JsonElement?,
                     typeOfT: Type?,
                     context: JsonDeserializationContext?
-                ): ImmutableList<Idiom> {
+                ): ImmutableList<Option> {
                     val idioms = json?.asJsonArray?.map { jsonElement ->
                         val jsonObject = jsonElement.asJsonObject
-                        val id = jsonObject.get("id").asInt
-                        val category = jsonObject.get("category").asInt
                         val chineseCharacters = jsonObject.get("chineseCharacters").asString
                         val chineseMeanings = jsonObject.get("chineseMeanings").asJsonArray.map { it.asString }
                         val description = jsonObject.get("description").asString
                         val koreanCharacters = jsonObject.get("koreanCharacters").asString
-                        val my = jsonObject.get("my").asBoolean
 
-                        Idiom(
-                            id = id,
-                            category = category,
-                            chineseCharacters = chineseCharacters,
-                            chineseMeanings = chineseMeanings.toImmutableList(),
-                            description = description,
-                            koreanCharacters = koreanCharacters,
-                            my = my
-                        )
+                        object : Option() {
+                            override val chineseCharacters: String = chineseCharacters
+                            override val chineseMeanings: ImmutableList<String> = chineseMeanings.toImmutableList()
+                            override val description: String = description
+                            override val koreanCharacters: String = koreanCharacters
+                        }
                     } ?: emptyList()
 
                     return idioms.toImmutableList()
