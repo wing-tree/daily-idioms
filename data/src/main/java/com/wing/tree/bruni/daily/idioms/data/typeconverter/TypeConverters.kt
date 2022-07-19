d026b189bcf058c00264a53f90f437cdbeb12d2c
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-19 10:40:00 +0900

    feat : Create custom deserializer for ImmutableList<Idiom> and clean up

--- data/src/main/java/com/wing/tree/bruni/daily/idioms/data/typeconverter/TypeConverters.kt
+++ data/src/main/java/com/wing/tree/bruni/daily/idioms/data/typeconverter/TypeConverters.kt
@@ -1,31 +1,23 @@
 package com.wing.tree.bruni.daily.idioms.data.typeconverter
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
+import com.google.gson.*
 import com.google.gson.reflect.TypeToken
 import com.wing.tree.bruni.daily.idioms.data.entity.Idiom
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
+import java.lang.reflect.Type
 
 class TypeConverters {
     @Suppress("HasPlatformType")
     @TypeConverter
-    fun idiomToJson(value: Idiom) = Gson().toJson(value)
+    fun immutableStringsToJson(value: ImmutableList<String>) = Gson().toJson(value)
 
     @TypeConverter
-    fun jsonToIdiom(value: String): Idiom {
-        return Gson().fromJson(value, Idiom::class.java)
-    }
-
-    @Suppress("HasPlatformType")
-    @TypeConverter
-    fun stringsToJson(value: List<String>) = Gson().toJson(value.toList())
-
-    @TypeConverter
-    fun jsonToStrings(value: String): List<String> {
+    fun jsonToImmutableStrings(value: String): ImmutableList<String> {
         val type = object : TypeToken<List<String>>() {  }.type
 
-        return Gson().fromJson(value, type)
+        return Gson().fromJson<List<String>>(value, type).toImmutableList()
     }
 
     @Suppress("HasPlatformType")
@@ -34,8 +26,42 @@ class TypeConverters {
 
     @TypeConverter
     fun jsonToImmutableIdioms(value: String): ImmutableList<Idiom> {
-        val type = object : TypeToken<List<Idiom>>() {  }.type
+        val type = object : TypeToken<ImmutableList<Idiom>>() {  }.type
 
-        return Gson().fromJson<List<Idiom>>(value, type).toImmutableList()
+        val gson = GsonBuilder().registerTypeAdapter(
+            type,
+            object : JsonDeserializer<ImmutableList<Idiom>> {
+                override fun deserialize(
+                    json: JsonElement?,
+                    typeOfT: Type?,
+                    context: JsonDeserializationContext?
+                ): ImmutableList<Idiom> {
+                    val idioms = json?.asJsonArray?.map { jsonElement ->
+                        val jsonObject = jsonElement.asJsonObject
+                        val id = jsonObject.get("id").asInt
+                        val category = jsonObject.get("category").asInt
+                        val chineseCharacters = jsonObject.get("chineseCharacters").asString
+                        val chineseMeanings = jsonObject.get("chineseMeanings").asJsonArray.map { it.asString }
+                        val description = jsonObject.get("description").asString
+                        val koreanCharacters = jsonObject.get("koreanCharacters").asString
+                        val my = jsonObject.get("my").asBoolean
+
+                        Idiom(
+                            id = id,
+                            category = category,
+                            chineseCharacters = chineseCharacters,
+                            chineseMeanings = chineseMeanings.toImmutableList(),
+                            description = description,
+                            koreanCharacters = koreanCharacters,
+                            my = my
+                        )
+                    } ?: emptyList()
+
+                    return idioms.toImmutableList()
+                }
+            }
+        ).create()
+
+        return gson.fromJson(value, type)
     }
 }
