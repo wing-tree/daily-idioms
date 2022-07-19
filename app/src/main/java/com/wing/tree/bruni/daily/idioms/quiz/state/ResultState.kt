1567994b6a57a776197122b9ae4e1445393e139b
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-19 13:37:04 +0900

    feat : Add index property in ResultState

--- app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/state/ResultState.kt
+++ app/src/main/java/com/wing/tree/bruni/daily/idioms/quiz/state/ResultState.kt
@@ -3,6 +3,8 @@ package com.wing.tree.bruni.daily.idioms.quiz.state
 import com.wing.tree.bruni.daily.idioms.quiz.model.Question
 
 data class ResultState(val question: Question) {
+    val index = question.index
+
     val answer = question.answer
     val isCorrectAnswer = answer == question.correctAnswer
 }
